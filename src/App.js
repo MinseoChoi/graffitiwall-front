ef2351dbbe488e7f6b3ad15c708cb05e737541ae
sanggyunbak2856 <73566382+sanggyunbak2856@users.noreply.github.com>
2023-03-13 12:56:01 +0900

    Feat : 라우팅 기능 추가 (#1)

--- src/App.js
+++ src/App.js
@@ -2,66 +2,25 @@ import './App.css';
 import Header from './component/header/Header';
 import Sidebar from './component/sidebar/Sidebar';
 import CreatePostit from './component/createPostit/CreatePostit';
-import Canvasboard from './component/canvas/Canvasboard';
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  // const [ctx, setCtx] = useState();
-  // const canvasRef = useRef(null);
-
-  // const array = []
-  // useEffect(() => {
-  //   const canvas = canvasRef.current;
-  //   const context = canvas.getContext('2d');
-  //   context.lineJoin = 'round';
-  //   context.lineWidth = 3;
-  //   context.strokeStyle = 'blue'
-  //   setCtx(context);
-  // }, [])
-
-  // const canvasEventListener = (event, type) => {
-  //   let x = event.clientX - event.target.offsetLeft;
-  //   let y = event.clientY - event.target.offsetTop;
-  //   if (type === 'move') {
-  //     if (array.length === 0) {
-  //       array.push({ x, y })
-  //     } else {
-  //       ctx.save()
-  //       ctx.beginPath();
-  //       ctx.moveTo(array[array.length - 1].x, array[array.length - 1].y);
-  //       ctx.lineTo(x, y);
-  //       ctx.closePath();
-  //       ctx.stroke()
-  //       ctx.restore();
-  //       array.push({ x, y })
-  //     }
-  //   } else if (type === 'leave') {
-  //     ctx.save()
-  //     ctx.clearRect(0, 0, 2500, 2500)
-  //     ctx.restore()
-  //   }
-  // }
   return (
-    <div className="App">
-      <Header />
-      <div className="App-body">
-        <Sidebar />
-        <div className="main">
-          {/* <canvas ref={canvasRef} id="canvas" style={{ width: "80%", height: "77vh" }}
-              onMouseDown = {(event) => { canvasEventListener(event, 'down') }}
-              onMouseMove = {(event) => { canvasEventListener(event, 'move') }}
-              onMouseLeave = {(event) => { canvasEventListener(event, 'leave') }}
-              onMouseUp = {(event) => { canvasEventListener(event, 'up') }}>
-          </canvas> */}
-          {/* <PostitForm /> */}
-          <CreatePostit />
-          {/* <Canvasboard /> */}
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <div className="App-body">
+          <Sidebar />
+          <div className="main">
+            <Routes>
+              <Route path="/" element={<CreatePostit />}></Route>
+              <Route path="/board/:boardId" element={<CreatePostit />}></Route>
+            </Routes>
+          </div> 
         </div>
-        {/* <div className='BoardByPopularity'>
-          xxx
-        </div> */}
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
--- src/component/boardList/BoardList.js
+++ src/component/boardList/BoardList.js
@@ -1,36 +1,41 @@
 import './BoardList.css';
-// import { useNavigate  } from 'react-router-dom';
+import { useNavigate  } from 'react-router-dom';
 
 const BoardList = () => {
     const data = [
         {
+            id: '1',
             name: 'board1'
         },
         {
+            id: '2',
             name: 'board2'
         },
         {
+            id: '3',
             name: 'board3'
         },
         {
+            id: '4',
             name: 'board4'
         },
         {
+            id: '5',
             name: 'board5'
         },
     ];
-
-    // const navigate = useNavigate();
-    // const handleClick = (boardName) => {
-    //     navigate(`/${boardName}`);
-    // }
+    
+    const navigate = useNavigate();
+    const handleClick = (boardName) => {
+        navigate(`/board/${boardName}`);
+    }
 
     return (
         <div className="boardList">
             <p>Board List</p>
             <ul>
                 {data.map(element =>
-                    <li key={element.name}>
+                    <li key={element.id} onClick={() => handleClick(element.id)}>
                         {element.name}
                     </li>
                 )}
